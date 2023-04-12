049e87b36345a6cb5f0d6bf67d282a8e932371e1
fernandoGalarcio <fdowannabe@gmail.com>
2023-04-12 02:11:49 -0500

    route adjustment

--- pages/search/[q].tsx
+++ pages/search/[q].tsx
@@ -3,13 +3,26 @@ import Search from '@/src/components/elements/Search';
 import SearchResults from '@/src/components/elements/SearchResults';
 import { searchResultsState } from '@/src/libs/utils';
 import { useAtom } from 'jotai';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 const SearchPage = () => {
     return (
         <>
-        div
-            <Search />
+            <div className='flex items-center'>
+                <Link
+                    href={`/`}
+                    passHref
+                    legacyBehavior>
+                    <a
+                        className='mr-3'>
+                        <span>Fdo Bing Search</span>
+                    </a>
+                </Link>
+                <div className='w-1/3'>
+                    <Search />
+                </div>
+            </div>
             <SearchResults />
         </>
     );
--- src/components/elements/SearchResults.tsx
+++ src/components/elements/SearchResults.tsx
@@ -3,31 +3,24 @@ import { searchResultsState } from "@/src/libs/utils";
 import { useQuery } from "@tanstack/react-query";
 import { useAtom } from "jotai";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-
 const SearchResults: React.FC = () => {
     const router = useRouter();
     const { q } = router.query;
     console.log('q:', q);
+    const { data, isLoading, error } = useQuery(['searchResults', q], () => getSearchResults(q as string));
 
-    const [results, setResults ] = useState([]);
-
-
-
-
-    // console.log('data: ', data);
+    console.log('data: ', data);
     return (
-        q ? <div>
+        <div>
             {
-                results && results?.map((result: any) => (
+                data?.webPages && data?.webPages?.value?.map((result: any) => (
                     <div key={result?.id} className='border border-white my-2'>
                         <h2>{result?.name}</h2>
                         <a href={result?.url}>{result.url}</a>
                         <p>{result?.snippet}</p>
                     </div>
                 ))}
-        </div> : null
+        </div>
     )
 }
-
 export default SearchResults;
