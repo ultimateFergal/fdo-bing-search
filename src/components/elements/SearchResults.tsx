fb6c3db40a209903fa478d56d32cef035aa2a9a8
fernandoGalarcio <fdowannabe@gmail.com>
2023-04-12 01:40:55 -0500

    routing adjustment

--- src/components/elements/SearchResults.tsx
+++ src/components/elements/SearchResults.tsx
@@ -3,15 +3,22 @@ import { searchResultsState } from "@/src/libs/utils";
 import { useQuery } from "@tanstack/react-query";
 import { useAtom } from "jotai";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const SearchResults: React.FC = () => {
     const router = useRouter();
     const { q } = router.query;
     console.log('q:', q);
-    const { data, isLoading, error } = useQuery(['searchResults', q ?? ''], () => getSearchResults(q as string));
-    const results = data ? data?.webPages?.value : [];
+    // const { data, isLoading, error } = useQuery(['searchResults', q ?? ''], () => getSearchResults(q as string));
+    // const results = data ? data?.webPages?.value : [];
+    const [results, setResults ] = useState([]);
 
-    console.log('data: ', data);
+    useEffect(() => {
+        getSearchResults(q as string).then(data => setResults(data?.webPages?.value))
+    }, [q]);
+
+
+    // console.log('data: ', data);
     return (
         q ? <div>
             {
