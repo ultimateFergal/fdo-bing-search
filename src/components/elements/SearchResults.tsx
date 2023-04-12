216384eaf9a9da16acef5a92fccc7c5d39c906d2
fernandoGalarcio <fdowannabe@gmail.com>
2023-04-12 00:35:36 -0500

    adjustment

--- src/components/elements/SearchResults.tsx
+++ src/components/elements/SearchResults.tsx
@@ -14,7 +14,7 @@ const SearchResults: React.FC = () => {
     return (
         <div>
             {
-                data?.webPages && data?.webPages?.value?.map((result: any) => (
+                data && data?.webPages && data?.webPages?.value?.map((result: any) => (
                     <div key={result?.id} className='border border-white my-2'>
                         <h2>{result?.name}</h2>
                         <a href={result?.url}>{result.url}</a>
